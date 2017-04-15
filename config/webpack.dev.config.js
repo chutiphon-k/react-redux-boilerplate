1a45befead4e986319c853385c0c77e1b3e70a2e
Chutiphon.k <chutiphon.k@ku.th>
2017-04-16 02:44:02 +0700

    :wrench: Move config NODE_ENV to package.json

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,5 +1,3 @@
-process.env.NODE_ENV = 'development'
-
 const webpack = require('webpack')
 const path = require('path')
 const merge = require('webpack-merge')
--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,5 +1,3 @@
-process.env.NODE_ENV = 'production'
-
 const webpack = require('webpack')
 const path = require('path')
 const merge = require('webpack-merge')
