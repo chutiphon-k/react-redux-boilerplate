6053c789fbc2542fa6a325989b980657341ed6b6
Chutiphon.k <chutiphon.k@ku.th>
2017-04-17 06:34:38 +0700

    :hammer: Refactor routes

--- src/routes.js
+++ src/routes.js
@@ -6,27 +6,21 @@ import {
 } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 
+function errorLoading (error) {
+	throw new Error(`Dynamic page loading failed: ${error}`)
+}
+
+function loadRoute (cb) {
+	return component => cb(null, component.default || component)
+}
+
 const routes = (store, history) => (
 	<Router history={syncHistoryWithStore(history, store)}>
 		<Route path='/'
-			getComponent = { (location, cb) => {
-				System.import('containers/App')
-					.then((component) => {
-						cb(null, component.default || component)
-					})
-			}}>
-			<IndexRoute getComponent = { (location, cb) => {
-				System.import('containers/Home')
-					.then((component) => {
-						cb(null, component.default || component)
-					})
-			}} />
-			<Route path='about' getComponent = { (location, cb) => {
-				System.import('containers/About')
-					.then((component) => {
-						cb(null, component.default || component)
-					})
-			}} />
+			getComponent = {(location, cb) => { System.import('containers/App').then(loadRoute(cb)).catch(errorLoading) }}>
+			<IndexRoute getComponent = {(location, cb) => { System.import('containers/Home').then(loadRoute(cb)).catch(errorLoading) }} />
+			<IndexRoute getComponent = {(location, cb) => { System.import('containers/Home').then(loadRoute(cb)).catch(errorLoading) }} />
+			<Route path='about' getComponent = {(location, cb) => { System.import('containers/About').then(loadRoute(cb)).catch(errorLoading) }} />
 		</Route>
 	</Router>
 )
